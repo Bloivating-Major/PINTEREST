543079641cc43dd01bcfe2b94f6bf95f0c1b17ab
Bloivating-Major <sambhav12wakhariya@gmail.com>
2025-07-01 23:30:00 -0400

    ImageKit Integrated

---
+++ frontend/src/components/image/image.jsx
@@ -0,0 +1,22 @@
+import { Image as IKImage } from '@imagekit/react';
+
+const Image = ({ path, src, alt, className, w, h }) => {
+  return (
+    <IKImage
+      urlEndpoint={import.meta.env.VITE_URL_IK_ENDPOINT}
+      path={path}
+      src={src}
+      transformation={[
+        {
+          height: h,
+          width: w,
+        },
+      ]}
+      alt={alt}
+      loading="lazy"
+      className={className}
+    />
+  );
+};
+
+export default Image;
